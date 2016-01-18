1b8ff90df915fe3d1a473c76c057af50e61b141f
kehontas <kehontas@gmail.com>
2016-01-18 13:32:14 -0800

    models and controller set up and route to users index page set up as well initial view for all users

---
+++ trial1/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all 
+
+    render :index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
