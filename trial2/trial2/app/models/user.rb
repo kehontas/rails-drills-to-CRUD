a54ccca884c34743b9eff71ca61a486662bd9a98
kehontas <kehontas@gmail.com>
2016-01-18 19:38:25 -0800

    debuggin sign in error - undefined method confirm in SessContr

--- trial2/trial2/app/models/user.rb
+++ trial2/trial2/app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
 	has_secure_password
+
+	class User < ActiveRecord::Base
+  has_secure_password
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
+end
+
 end
