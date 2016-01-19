1ac27cd0334b43509d50a8eab736daec50aba029
kehontas <kehontas@gmail.com>
2016-01-18 19:44:05 -0800

    user actions complete

--- trial2/trial2/app/controllers/sessions_controller.rb
+++ trial2/trial2/app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
-
-  def new
+def new
     @user = User.new
     render :new
   end
-
   def create
     user_params = params.require(:user).permit(:email, :password)
     @user = User.confirm(user_params)
     if @user
       login(@user)
+      flash[:notice] = "Successfully logged in."
       redirect_to @user
     else
+      flash[:error] = "Incorrect email or password."
       redirect_to login_path
     end
-
-    def destroy
-      logout # this method lives in the SessionsHelper!
-      redirect_to root_path
+  end
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Successfully logged out."        # <--- Add this flash notice
+    redirect_to root_path
     end
 
-  end
- end
+
+end
--- trial2/trial2/app/models/user.rb
+++ trial2/trial2/app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
-	class User < ActiveRecord::Base
-  has_secure_password
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
@@ -10,4 +8,3 @@ class User < ActiveRecord::Base
   end
 end
 
-end
